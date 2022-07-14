05d60a5d985454ad79cec88fa3b414fe42c744f2
SFlorian95 <subramanyam.florian@outlook.fr>
2022-07-14 19:50:18 +0200

    Integration webpack

---
+++ src/scripts/components/api.js
@@ -0,0 +1,13 @@
+const axios = require('axios')
+
+const url = 'src/data/photographers.json'
+
+/**
+ * Get all photographers
+ * @returns Array of photographer object
+ */
+const getPhotographers = () => axios.get(url).then(response => response.data.photographers)
+
+module.exports = {
+    getPhotographers
+}
---
+++ src/scripts/pages/photographer.js
@@ -0,0 +1,4 @@
+//Mettre le code JavaScript lié à la page photographer.html
+module.exports = id => {
+    console.log(id)
+}
