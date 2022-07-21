a0226a3c5e4a7bceca5704675d7a5877636ca864
SFlorian95 <subramanyam.florian@outlook.fr>
2022-07-21 19:24:35 +0200

    integration de la page d'accueil

--- src/scripts/factories/photographer.js
+++ src/scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 module.exports = {
   create (data) {
-    const { name, portrait } = data
+    const { name, portrait, id, city, country } = data
 
     const picture = `/src/assets/photographers/${portrait}`
+    const url = `./scripts/pages/photographer/?id=${id}`
 
     const getUserCardDOM = () => {
       const article = document.createElement('article')
+
+      const a = document.createElement('a')
+      a.setAttribute('href', url)
+
+      const cityText = document.createElement('span')
+      cityText.textContent = city
+
+      const countryText = document.createElement('span')
+      countryText.textContent = country
+
       const img = document.createElement('img')
       img.setAttribute('src', picture)
+
       const h2 = document.createElement('h2')
       h2.textContent = name
-      article.appendChild(img)
-      article.appendChild(h2)
+
+      article.appendChild(a)
+      article.appendChild(cityText)
+      article.appendChild(countryText)
+      a.appendChild(img)
+      a.appendChild(h2)
       return (article)
     }
 
