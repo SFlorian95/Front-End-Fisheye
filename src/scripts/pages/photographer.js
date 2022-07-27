11e31b23b8fc7c27226f5a7cc9eadf5173146e60
SFlorian95 <subramanyam.florian@outlook.fr>
2022-07-27 17:06:56 +0200

    working progress

--- src/scripts/pages/photographer.js
+++ src/scripts/pages/photographer.js
@@ -1,4 +1,4 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Mettre le code JavaScript lié à la page photographer.html
 module.exports = id => {
-    console.log(id)
+  console.log(id)
 }
